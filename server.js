555bef6e4b780be0b7a1e847b242ab8e0d358595
bhaskar_bisht <bisht3182568@gmail.com>
2026-03-01 15:54:03 +0530

    update frontend url

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ const connectDB = require("./config/db");
 
 const app = express();
 
-app.use(cors());
+app.use(
+    cors({
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+    }),
+);
 app.use(express.json());
 
 // Routes
